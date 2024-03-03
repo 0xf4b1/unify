efc82fe29a249efb6d20ffc08fd77e20ef38a4ee
bibo38 <bibo38@github.com>
2024-03-03 01:00:42 +0100

    Improved error reporting
    Every error now has a separate exitcode. Also removed the stat calls
    as they are unnecessary and only produce error messages when they cannot
    find a given folder (which don't indicate an error)

--- unify.sh
+++ unify.sh
@@ -4,46 +4,46 @@ UNITY_REPO="${UNITY_REPO:-"$HOME/Unity/Hub/Editor"}"
 UNITY_VARIANT="${UNITY_VARIANT:-linux64_withgfx_nondevelopment_mono}"
 UNITY_ENGINE_PREFIX="${UNITY_ENGINE_PREFIX:-Editor/Data/PlaybackEngines/LinuxStandaloneSupport/Variations}"
 
+die() {
+	CODE=$1
+	shift
+	>&2 echo $*
+	exit $CODE
+}
+
 echo $1
 
 # Determine Unity engine version
 res=`find "$1" -name level0`
-[ -z "$res" ] && echo "Can not find level0 file" && exit 1
+[ -z "$res" ] && die 1 Cannot find level0 file
 version=`strings "$res" | head -n 1`
 echo "Found Unity version: $version"
 
 # Probe for opengl renderer
 DATA_DIR=`dirname "$res"`
-strings "$DATA_DIR/Resources/unity_builtin_extra" | grep gl_ >/dev/null
-[ ! $? -eq 0 ] && echo "OpenGL renderer not enabled :(" && exit 1
+strings "$DATA_DIR/Resources/unity_builtin_extra" | grep gl_ >/dev/null || die 2 OpenGL renderer not enabled ':('
 
 # Check Unity Engine
 UNITY_PATH="$UNITY_REPO/$version"
-stat "$UNITY_PATH/$UNITY_ENGINE_PREFIX/$UNITY_VARIANT" >/dev/null
-if [ ! $? -eq 0 ]; then
-  stat "$UNITY_PATH/Unity.tar.xz" >/dev/null
-  if [ ! $? -eq 0 ]; then
-    echo "Unity version not found"
-    stat "$UNITY_REPO/archive" >/dev/null
-    if [ ! $? -eq 0 ]; then
+if [ ! -d "$UNITY_PATH/$UNITY_ENGINE_PREFIX/$UNITY_VARIANT" ]; then
+  if [ ! -f "$UNITY_PATH/Unity.tar.xz" ]; then
+    echo "Unity version not found locally"
+    if [ ! -f "$UNITY_REPO/archive" ]; then
         echo "Fetching Unity archive ..."
         mkdir -p "$UNITY_REPO"
-        curl https://unity.com/releases/editor/archive -o "$UNITY_REPO/archive"
-        [ ! $? -eq 0 ] && echo "Could not fetch Unity archive" && exit 1
+        curl https://unity.com/releases/editor/archive -o "$UNITY_REPO/archive" || die 3 Could not fetch Unity archive
     fi
     res=`cat $UNITY_REPO/archive | grep unityhub://$version`
-    [ -z "$res" ] && echo "Unity version not found in archive" && exit 1
+    [ -z "$res" ] && die 4 Unity version not found in archive
     HASH=${res##*$version/}
     HASH=${HASH%%\"*}
     mkdir "$UNITY_REPO/$version"
-    curl "https://download.unity3d.com/download_unity/$HASH/LinuxEditorInstaller/Unity.tar.xz" -o "$UNITY_PATH/Unity.tar.xz"
-    [ ! $? -eq 0 ] && echo "Could not fetch Unity engine archive" && exit 1
+    curl "https://download.unity3d.com/download_unity/$HASH/LinuxEditorInstaller/Unity.tar.xz" -o "$UNITY_PATH/Unity.tar.xz" || die 5 Could not fetch Unity engine archive
   fi
   echo "Extracting $UNITY_VARIANT from Unity.tar.xz"
   tar -xf "$UNITY_PATH/Unity.tar.xz" -C "$UNITY_PATH" "$UNITY_ENGINE_PREFIX/$UNITY_VARIANT"
 fi
 
-cp -r "$UNITY_PATH/$UNITY_ENGINE_PREFIX/$UNITY_VARIANT"/* "$DATA_DIR/../"
-[ ! $? -eq 0 ] && echo "Could not copy Unity Engine files" && exit 1
+cp -r "$UNITY_PATH/$UNITY_ENGINE_PREFIX/$UNITY_VARIANT"/* "$DATA_DIR/../" || die 6 Could not copy Unity Engine files
 
 echo "Unity engine files copied."
